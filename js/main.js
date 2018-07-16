97f9c1713981814a9e64f70452203adcced9a768
eduardo <eduardosilva-9@hotmail.com>
2018-07-16 18:20:11 -0300

    Primeiro comite

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+// Iniciar os plugins
+$(document).ready(function(){
+	
+	//inicializar apresentação de slides
+	 $('.flexslider').flexslider({
+        animation: "slide",
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+
+});
+/**
+ * Alças alternando o menu de navegação para telas pequenas.
+ */
+( function() {
+	var button = document.getElementById( 'topnav' ).getElementsByTagName( 'div' )[0],
+	    menu   = document.getElementById( 'topnav' ).getElementsByTagName( 'ul' )[0];
+
+	if ( undefined === button )
+		return false;
+
+	// Ocultar botão se o menu estiver faltando ou vazio.
+	if ( undefined === menu || ! menu.childNodes.length ) {
+		button.style.display = 'none';
+		return false;
+	}
+
+	button.onclick = function() {
+		if ( -1 == menu.className.indexOf( 'srt-menu' ) )
+			menu.className = 'srt-menu';
+
+		if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
+			button.className = button.className.replace( ' toggled-on', '' );
+			menu.className = menu.className.replace( ' toggled-on', '' );
+		} else {
+			button.className += ' toggled-on';
+			menu.className += ' toggled-on';
+		}
+	};
+} )();
